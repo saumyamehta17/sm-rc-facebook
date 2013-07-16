8be922ea2cdb63d5ee8cc69779d025372def1a65
sweety <smehta1289@gmail.com>
2013-07-16 20:17:24 +0530

    omniauth for twitter

---
+++ README.md
@@ -0,0 +1,24 @@
+SM RailsCast 235
+================
+
+Omniauth..
+ is a way to do user authentication through third party provider such as facebook or twitter .
+
+Setup steps
+------------
+
+Clone repository
+
+```
+git clone 
+
+git clone https://github.com/sweetymehta/sm-rc235.git
+```
+Go to app's folder
+```
+cd sm-rc235
+```
+add gem devise and install and create scaffolding..
+```
+#todo..
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc235::Application.config.secret_token = '77fec89cb39504f948709d7238c7cfd88530045e7c09f77055ad0bc9a35cc0a2c7dbfd7e8185f1aec6efdb0df84439064b5aa6033c05f34d0bc46a63e6b96c40'
