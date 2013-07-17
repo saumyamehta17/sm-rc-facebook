12ad21408fc1a396787b65748abac9c588114d1a
sweety <smehta1289@gmail.com>
2013-07-17 15:27:17 +0530

    wid route error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :developer unless Rails.env.production?
+  	
+ 
+  provider :twitter, ENV['9PedQmtfkYVQHHqvUiQLfA'], ENV['swUYHtmB1F5fybAZ2F58sG7lJCJtBwf6cWfQp48g7Q']
+
 end
